5c631ed2ec2bb1a641f11b6e554fa088fce1c471
eyosialex <Eyosialex27@gmail.com>
2026-07-27 22:59:45 +0300

    add the model of book

---
+++ model/book.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self,author,title,page):
+        self.author=author;
+        self.title=title;
+        self.page=page;
+    def display(self):
+        print("author"+self.author)
+        print ("title"+self.title)
+        print("page"+self.page)
+    def is_big(self):
+        return page>500
+
+        
