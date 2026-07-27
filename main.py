1bb07bc43ad1f1dc8036a2263cd718c20cf1fced
eyosialex <Eyosialex27@gmail.com>
2026-07-28 00:01:12 +0300

    the borrow implementation

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     student1= Student("eyosi",23,"software",3.4)
     print ("the available books ")
     book1.display()
+    book1.borrow()
+    book1.display()
     print ( "my big book is: ")
     book1.is_big()
     print("student info :" ,student1.display())
