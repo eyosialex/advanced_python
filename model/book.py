291866c1e0b55f41f5361f0e8fa01aea7492ee9e
eyosialex <Eyosialex27@gmail.com>
2026-07-28 00:00:40 +0300

    add borrow function

--- model/book.py
+++ model/book.py
@@ -1,13 +1,28 @@
 class Book:
+    totalbook=0;
     def __init__(self,author,title,page):
         self.author=author;
         self.title=title;
         self.page=page;
+        self.available=True
+        Book.totalbook=+1;
     def display(self):
         print("author: "+self.author)
         print ("title: "+self.title)
         print("page: ",self.page)
     def is_big(self):
         return self.page>500
+    def borrow (self):
+        if self.available:
+            self.available=False
+            Book.totalbook=-1
+            print (f"print the book is borrow ")
+        else:
+            print(f"print the book is already borrowed")
+    def return_book(self):
+        self.available=true
+        print ( "the book {self.name} has ben returned")
+        
+
 
         
