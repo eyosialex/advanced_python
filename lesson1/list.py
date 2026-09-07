162493b2f0de613c3bc5b3e792a6a50f0e693b4e
Eyosiyas Alemayehu <eyosialex27@gmail.com>
2026-09-07 18:58:41 +0300

    add some operations on list

--- lesson1/list.py
+++ lesson1/list.py
@@ -19,4 +19,24 @@ cubes[3] = 64
 [1, 8, 27, 64, 125]
 cubes.append(216) # add the cube of 6cubes.append(7 ** 3) # and the cube of 7
 cubes
-[1, 8, 27, 64, 125, 216, 343]
+#[1, 8, 27, 64, 125, 216, 343]
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+letters
+#['a', 'b', 'c', 'd', 'e', 'f', 'g']
+ # replace some values
+letters[2:5] = ['C', 'D', 'E']
+ letters
+#['a', 'b', 'C', 'D', 'E', 'f', 'g']
+# now remove them
+letters[2:5] = []
+ letters
+#['a', 'b', 'f', 'g']
+# clear the list by replacing all the elements with an empty list
+letters[:] = []
+letters
+
+#[]
+# The built-in function len() also applies to lists:
+letters = ['a', 'b', 'c', 'd']
+len(letters)
+#4
