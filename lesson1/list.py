d790e3146deebb99f546502e1b9397ab37e1f03d
Eyosiyas Alemayehu <eyosialex27@gmail.com>
2026-09-07 18:47:39 +0300

    lists are a mutable type, i.e. it is possible to change their content:

--- lesson1/list.py
+++ lesson1/list.py
@@ -8,3 +8,12 @@ squares[-1]
 #Lists also support operations like concatenation:
 squares + [36, 49, 64, 81, 100]
 [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+#Unlike strings, which are immutable, lists are a mutable type, i.e. it is possible to change their content:
+cubes = [1, 8, 27, 65, 125] # something's wrong here
+4 ** 3
+ # the cube of 4 is 64, not 65!
+64
+cubes[3] = 64 
+# replace the wrong valuecubes
+[1, 8, 27, 64, 125]
