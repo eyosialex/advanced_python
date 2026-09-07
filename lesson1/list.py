0ba2bb05112b1b5ffbe0d8c2f87f58b720fa6270
Eyosiyas Alemayehu <eyosialex27@gmail.com>
2026-09-07 18:44:22 +0300

    Lists also support operations like concatenation:

--- lesson1/list.py
+++ lesson1/list.py
@@ -5,3 +5,6 @@ squares[0] # indexing returns the item
 # 1
 squares[-1]
 # 25
+#Lists also support operations like concatenation:
+squares + [36, 49, 64, 81, 100]
+[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
